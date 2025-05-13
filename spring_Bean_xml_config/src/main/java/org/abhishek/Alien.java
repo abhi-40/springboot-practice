1c040e6844f7ce271f2b5d3c49d5c6a93258bfd5
Abhishek Yadav <abhiy01159@gmail.com>
2025-05-13 11:32:30 +0530

    Add files via upload
    Consider this as an example of dependency injection in spring framework.
    
    Basically, in this we are Creating Beans(objects) by using xml based configuration in spring framework .

---
+++ spring_Bean_xml_config/src/main/java/org/abhishek/Alien.java
@@ -0,0 +1,9 @@
+package org.abhishek;
+
+public class Alien
+{
+    public void code()
+    {
+        System.out.println("Coding");
+    }
+}
---
+++ spring_Bean_xml_config/src/main/java/org/abhishek/App.java
@@ -0,0 +1,14 @@
+package org.abhishek;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        ApplicationContext context=new ClassPathXmlApplicationContext("spring.xml");
+        Alien obj=(Alien)context.getBean("Alien");
+        obj.code();
+    }
+}

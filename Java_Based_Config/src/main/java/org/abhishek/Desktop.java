913e85543f7d7d9dd98bfd01cfbd40a8fac7ea4f
Abhishek Yadav <abhiy01159@gmail.com>
2025-05-21 14:59:52 +0530

    Add files via upload
    Creating Beans  in spring Via java based configuration i.e : By Using Annotations

---
+++ Java_Based_Config/src/main/java/org/abhishek/Desktop.java
@@ -0,0 +1,9 @@
+package org.abhishek;
+
+public class Desktop
+{
+    public void compile()
+    {
+        System.out.println("Compiling using Desktop");
+    }
+}
---
+++ Java_Based_Config/src/main/java/org/abhishek/config/AppConfig.java
@@ -0,0 +1,16 @@
+package org.abhishek.config;
+import org.abhishek.Desktop;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig
+{
+    @Bean
+
+    public Desktop desktop()
+    {
+        return new Desktop();
+    }
+
+}
